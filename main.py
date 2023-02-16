a7b5c729c75f9b258d6748b37297c807c7b372fa
evgenyChudaev <45080469+evgenyChudaev@users.noreply.github.com>
2023-02-15 20:07:40 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -6,5 +6,19 @@ Created on Wed Feb 15 19:43:29 2023
 """
 
 import streamlit as st
+from joblib import load
+from sklearn import tree
 
 st.title('Test')
+LABELS = ['setosa', 'versicolor', 'virginica']
+
+clf = load("DT.joblib")
+
+sp_l = st.slider('sepal len (cm)', min_value = 0, max_value = 10)
+sp_w = st.slider('sepal width (cm)', min_value = 0, max_value = 10)
+pe_l = st.slider('pital len (cm)', min_value = 0, max_value = 10)
+pe_w = st.slider('pital width (cm)', min_value = 0, max_value = 10)
+
+prediction = clf.predict([[sp_l, sp_w, pe_l, pe_w]])
+
+st.write(LABELS[prediction][0])
